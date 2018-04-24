f62c01448f316a17274c6f87f131b40efe6190f3
Alexey Trifonov <avrong@outlook.com>
2018-04-24 21:46:44 +0300

    Add OS name

--- src/windows.rs
+++ src/windows.rs
@@ -1,12 +1,14 @@
 //! Information parser for windows systems
 
 use std::env;
+use std::process::Command;
 
 /// Windows information
 pub fn info() -> Vec<Vec<String>>{
     let mut info = Vec::new();
 
     info.push(vec!("".to_owned(), format!("{}@{}", get_username(), get_hostname())));
+    info.push(vec!("OS".to_owned(), get_system_name()));
 
     info
 }
@@ -21,8 +23,16 @@ fn get_hostname() -> String {
 
 /// Get name of current user
 fn get_username() -> String {
-    match env::var("COMPUTERNAME") {
+    match env::var("username") {
         Ok(desktop_name) => desktop_name,
+        Err(_) => "unknown".to_owned()
+    }
+}
+
+/// Get OS name
+fn get_system_name() -> String {
+    match Command::new("ver").output() {
+        Ok(ver) => String::from_utf8(ver.stdout).unwrap(),
         Err(_) => "unknown".to_owned()
     }
 }
